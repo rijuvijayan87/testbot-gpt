244219d2adc6c76a7811a63f1ed5e278111c781f
Riju Vijayan <rijuvijayan@gmail.com>
2023-05-22 23:04:07 +1200

    chore: vscode setting and isort dependency

--- testbot.py
+++ testbot.py
@@ -1,9 +1,39 @@
+import getopt
 import os
+import sys
+
 import openai
 from dotenv import load_dotenv
 
-
 load_dotenv()
-openai.api_key=os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print(f'apikey {openai.api_key}')
+
+def show_options() -> None:
+    """
+    This function displays different option about how this script can be used
+    """
+    user_prompt: str = "Usage: python testbot.py -i <inputfile> -o <outputfile>"
+
+    if len(sys.argv) != 2:
+        print(
+            f"Error! Invalid number of arguments provided. Correct {user_prompt}",
+            file=sys.stderr,
+        )
+        sys.exit(0)
+    try:
+        opts, _ = getopt.getopt(sys.argv[1:], "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print(user_prompt)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("Usage: python testbot.py -i <inputfile> -o <outputfile>")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            print(f"ifile name is : {arg}")
+        elif opt in ("-o", "--ofile"):
+            print(f"ofile name is : {arg}")
+
+
+show_options()
